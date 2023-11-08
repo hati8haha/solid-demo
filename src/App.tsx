bde82a0e5062ec55006134065e66597ea93c809d
HaoTing Cheng <haoting3462@gmail.com>
2023-11-08 18:07:44 +0000

    Update App.tsx and todo.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,6 @@ function App() {
 
   return (
     <div class="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://www.solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
       <Todo />
     </div>
   )
--- src/todo.tsx
+++ src/todo.tsx
@@ -3,20 +3,27 @@ import {createSignal, For} from 'solid-js'
 function Todo () {
   const [todos, setTodos] = createSignal<string[]>([])
   const [text, setText] = createSignal<string>('')
+  const handleInputChange = (e: Event & {
+    currentTarget: HTMLInputElement;
+    target: HTMLInputElement;
+  }) => {
+    setText(e.target.value)
+    console.log(e.target.value)
+  }
   return (
     <div>
         <h2>Todo list </h2>
         <div>
-          <input onChange={e => setText(e.target.value)} value={text()} onKeyDown={e => {
+          <input onChange={handleInputChange} value={text()} onkeypress={e => {
             if (e.key !== 'Enter' || text() === '') return
-            setTodos((prev) => [...prev, text()] )
+            setTodos((prev: string) => [...prev, text()])
             setText('')
             } } />
         </div>
         
         <ul>
           <For each={todos()} fallback={<li>Loading...</li>}>
-            {(item, i) => <li>{item} <button onClick={() => setTodos(prev => [...prev.slice(0, i()), ...prev.slice(i()+1)])}>delte</button> </li>}
+            {(item, i) => <li>{item} <button onClick={() => setTodos((prev: string[]) => [...prev.slice(0, i()), ...prev.slice(i()+1)])}>delte</button> </li>}
           </For>
         </ul>
     </div>
